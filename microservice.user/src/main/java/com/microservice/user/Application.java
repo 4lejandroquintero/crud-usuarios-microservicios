e4a3f0a2d566878f76a5ff4a0be4a3e903516c52
Alejandro Quintero <alejoq96@gmail.com>
2025-02-26 15:34:10 -0500

    fix(msvc-users): Se organiza el archivo pom.xml y errores de configuración en este microservicios, dado que no corria de manera adecuada

--- microservice.user/src/main/java/com/microservice/user/Application.java
+++ microservice.user/src/main/java/com/microservice/user/Application.java
@@ -3,9 +3,8 @@ package com.microservice.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
