01c58c89b838c851bc134eec2d974eb119aba6c0
Alejandro Quintero <alejoq96@gmail.com>
2025-02-26 19:40:07 -0500

    chore: Cambios de configuracion y dependencias

--- microservice.task/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tasks (title, description, status) VALUES ('Matemáticas', 'Profesor A', 'ASIGNADA');
-INSERT INTO tasks (title, description, status) VALUES ('Historia', 'Profesor B', 'EN_PROCESO');
-INSERT INTO tasks (title, description, status) VALUES ('Ciencias', 'Profesor C', 'ASIGNADA');
-INSERT INTO tasks (title, description, status) VALUES ('Literatura', 'Profesor D', 'EN_PROCESO');
-INSERT INTO tasks (title, description, status) VALUES ('Educación Física', 'Profesor E', 'FINALIZADA');
---
+++ microservice.user/src/main/java/com/microservice/user/exception/entities/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.microservice.user.exception.entities;
+
+public class EntityNotFoundException extends RuntimeException {
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+}
