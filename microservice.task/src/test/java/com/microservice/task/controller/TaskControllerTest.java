6a766b2b51f7e5d58cf8a5905ab5359a7f79911f
Alejandro Quintero <alejoq96@gmail.com>
2025-02-27 21:23:19 -0500

    choreAjuste global en dependencias

---
+++ microservice.task/src/test/java/com/microservice/task/controller/TaskControllerTest.java
@@ -0,0 +1,4 @@
+package com.microservice.task.controller;
+
+public class TaskControllerTest {
+}
--- microservice.user/src/test/java/com/microservice/user/service/UserServiceImplTest.java
+++ microservice.user/src/test/java/com/microservice/user/service/UserServiceImplTest.java
@@ -1,29 +1,4 @@
 package com.microservice.user.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-class UserServiceImplTest {
-
-    @org.junit.jupiter.api.Test
-    void findAll() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void findById() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void save() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void updateUser() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void deleteUser() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getUsersByRoles() {
-    }
+public class UserServiceImplTest {
 }
