7e14ea928e4a16806bca406d96986885dd7263c7
Alejandro Quintero <alejoq96@gmail.com>
2025-03-01 01:39:45 -0500

    fix(msvc-user): cambios en el codigo para incormporacion frontend

--- microservice.user/src/main/java/com/microservice/user/persistence/UserRepository.java
+++ microservice.user/src/main/java/com/microservice/user/persistence/UserRepository.java
@@ -3,13 +3,9 @@ package com.microservice.user.persistence;
 import com.microservice.user.entities.RolUser;
 import com.microservice.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
